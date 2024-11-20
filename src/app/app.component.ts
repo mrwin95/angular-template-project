5e26bd6039e53519aa188246382dd046562acdfa
Thang Nguyen <mrwin95@gmail.com>
2024-11-20 01:40:03 -0700

    complete roles page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RolesComponent } from './components/roles/roles.component';
+import { MasterComponent } from './components/master/master.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MasterComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-template-project';
